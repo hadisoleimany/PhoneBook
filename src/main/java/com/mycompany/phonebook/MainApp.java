d500eab0032c7316a43378cccffbf3539461df60
hsoleimany <hsoleimany@test.web>
2018-05-08 17:27:46 +0430

    FirstProject

---
+++ src/main/java/com/mycompany/phonebook/MainApp.java
@@ -0,0 +1,32 @@
+package com.mycompany.phonebook;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+ 
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new frmPhoneBook().setVisible(true);
+        launch(args);
+    }
+
+}
